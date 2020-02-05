48ad4a9221c4282fdff6c770d7d33020d30193c4
KonitzerTraining <mk@konitzer-training.de>
2020-02-05 10:12:36 +0100

    Symbol

---
+++ js_examples/src/12_generator/index.js
@@ -0,0 +1,43 @@
+// normale Funktion
+// Ein Aufruf => Ein Ergebnis
+
+function f1 () {
+    return 1;
+}
+
+let erg = f1();
+console.log(erg);
+
+// Generator
+function* f2 () {
+    let i = 0;
+    while (i < 4) {
+        i += 1;
+        yield i;
+    }
+}
+
+const gen = f2();
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+
+class Collection {
+    * [Symbol.iterator] () {
+        yield 1;
+        yield 2;
+    }
+}
+
+let c = new Collection();
+for (let entries of c) {
+    console.log(entries);
+}
+
+let list = [...c];
+console.log(list);
+
+let list2 = Array.from(c);
+console.log(list2);
