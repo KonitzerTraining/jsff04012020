97e5ccb3460fb0ec7ea45aaf2a539e7a2eb6b860
KonitzerTraining <mk@konitzer-training.de>
2020-02-06 15:16:36 +0100

    more js-examples

--- js_examples/src/10_map/index.js
+++ js_examples/src/10_map/index.js
@@ -1,7 +1,11 @@
 //ES6
+// Map ist wie ein Objekt, aber
+// kann als Schlüssel alles enthalten
 let map = new Map();
-
+map.
 console.log(Map.prototype);
+
+// map.set(key,value);
 map.set({id: 3, title: 'Haus'}, 2);
 map.set({id: 4, title: 'Baum'}, 1);
 map.set({id: 5, title: 'Moped'}, 7);
@@ -11,6 +15,7 @@ for (let [key, value] of map.entries()) {
     console.log(key, value);
 }
 
+
 for (let key of map.keys()) {
     console.log(key);
 }
@@ -19,11 +24,12 @@ for (let value of map.values()) {
     console.log(value);
 }
 
-map.forEach((item) => {
-    console.log(item);
+map.forEach((item, key) => {
+    console.log(key, item);
 });
 
 let iterable = map.entries();
+
 console.log(iterable.next());
 console.log(iterable.next());
 console.log(iterable.next());
