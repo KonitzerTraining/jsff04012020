4bac68d664ab252004bdf05e1a72e39ca1942a9d
KonitzerTraining <mk@konitzer-training.de>
2020-02-06 13:31:06 +0100

    more js-examples

--- js_examples/src/06_ES6Class/index.js
+++ js_examples/src/06_ES6Class/index.js
@@ -1,42 +1,51 @@
+// Super Class
 class Topic {
     constructor() {
         this.date = Date.now();
-
     }
 }
 
+// Sub Class
 class Project extends Topic {
     constructor() {
+        // Constructor Stealing
         super(); // Topic.call(this,...);
 
+        // private via Closure
         let id = Math.random();
+
         Reflect.defineProperty(this, 'id', {
             get () {
-                return id;
+                return id; // id ist Closure-Variable
             }
         });
 
+        // ES3-Property!!
         this.status = 'stopped';
     }
 
+    // Method landet in Project.prototype.start
     start () {
         this.status = 'running';
     }
+
+    // landet im prototype
     get something () {
         return 'anything';
     }
 
+    // Eigenschaft  Project.isProject
     static isProject (p) {
         return p.constructor === Project;
     }
 }
 
-console.log(typeof Project);
+console.log(typeof Project); // function
 console.dir(Project);
 
 let p1 = new Project();
-console.log(p1.id);
+console.log(p1.id); // get id wird aufgerufen
 
 console.log(p1);
 
-console.log(Project.isProject(p1));
+console.log(Project.isProject(p1)); // true
