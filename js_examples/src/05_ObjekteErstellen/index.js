5e630a89d4c77a4638cb92d9823cadc344227b41
KonitzerTraining <mk@konitzer-training.de>
2020-02-06 11:58:52 +0100

    more js-examples

--- js_examples/src/05_ObjekteErstellen/index.js
+++ js_examples/src/05_ObjekteErstellen/index.js
@@ -1,31 +1,38 @@
 'use strict';
 
 // ES 3
-let o1 = new Object();
+// Object als Konstruktorfunktion
+let o1 = new Object(); // unüblich
+console.log(o1);
 
-// Object Literal Notation
+// Object Literal Notation - besser als new Object()
 let o2 = {
     color: 'red',
     title: 'Text',
+    //Zeichenkette als Eigenschaft möglich
     'meine persönliche Eigenschaft': 'test',
+
+    // Number als Schlüssel, wie bei Arrays oder anderen Collections
     3: 'auch'
 };
+
+// Zusätzliche Eigenschaften
 o2.color = 'blue';
 o2.amount = 4;
 
+// Lesezugriff über . oder []
 console.log(o2.amount);
 console.log(o2['amount']);
 console.log(o2['meine persönliche Eigenschaft']);
 console.log(o2[3]);
 
-// Anonymer Block
-{
-    let a = 1;
-}
-
+// Löschen einer Eigenschaft möglich in (ES5) und höher
 delete o2.color;
 console.log(o2);
 
+
+// Iterieren über Objekt-Eigenschaften und
+// über die Eigenschaften im Prototyp (nicht gut).
 for (let key in o2) {
     console.log(key);
 }
@@ -38,6 +45,8 @@ Person.prototype.city = 'Berlin';
 let p1 = new Person();
 
 for (let key in p1) {
+
+    // Eigenschaften im Prototyp ausschließen
     if (p1.hasOwnProperty(key)) {
         console.log(key);
     }
@@ -52,7 +61,7 @@ oES5 = Object.create({
 }, {
     name: { // property descriptor map
         value: 'Hans',
-        enumerable: true, // schleifenfähig
+        enumerable: false, // schleifenfähig
         writable: true, // änderbar
         configurable: false // löschbar/änderbar
     },
@@ -67,11 +76,16 @@ for (let key in oES5) {
     console.log(key);
 }
 
+// Object.keys liefer alle Eigenschaften des Objects, wenn
+// diese enumerable, keine aus dem Prototype
 Object.keys(oES5).forEach((key) => {
     console.log(key, oES5[key]);
 });
 
+// kein neuen Eigenschaften hinzufügen oder vorhandene löschen
 //Object.seal(oES5);
+
+// keine Änderungen
 //Object.freeze(oES5);
 
 let product = {
@@ -104,12 +118,12 @@ let prop1 = 'plz';
 let f = 'sayHello';
 let prop3 = 'id';
 let oES6 = {
-    title,
-    makeReservation () {
+    title, // title: title
+    makeReservation() { // makeReservation: function () {
         console.log('ok');
     },
-    [prop1]: '09389',
-    [f] () {
+    [prop1]: '09389', // plz: '09389'
+    [f]() {
         console.log('hallo');
     },
     get [prop3]() {
@@ -126,8 +140,9 @@ oES6[f]();
 console.log(oES6.id);
 oES6.id = '234';
 
-Reflect.defineProperty(oES6, 'street',{
+Reflect.defineProperty(oES6, 'street', {
     value: 'soso', enumerable: true
-} );
+});
 
 console.log(oES6);
+oES6.street = 2234;
